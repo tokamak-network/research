6dea2c2ea54c9791f9e20607ff4bc0e4f5ae02b8
jjh2014 <67704672+jjh2014@users.noreply.github.com>
2020-08-30 18:39:57 +0900

    Code Update: Groth16 MATLAB Implementation
    R1CS and QAP have been implemented.
    Define your own (simple) function to prove.

--- MATLAB/Groth16/main.m
+++ MATLAB/Groth16/main.m
@@ -4,14 +4,259 @@
 % Contact: jjh2014@gist.ac.kr
 
 clear
-%% System Params
-p=71; % Choose any prime p such that mod(p,4)==3.
-q=p^2-1;
+%% System params
+p=71; % Field size for elliptic curve arithmetic. Choose any prime p such that mod(p,4)==3.
+q=p^2-1; % Field size for program, R1CS, and QAP.
 k=2;
 % Elliptic curve: y^2=x^3+a*x+0
 a=1;
 b=0;
 
+%% Program definition
+Num_In=2;
+Num_Out=1;
+c=sym('c',[1 Num_In]);
+y=sym('y',[1 Num_Out]);
+
+% Define program
+y(1)=(c(1)+c(2))*(c(1)-4);
+% If the computational size of program is large,
+% this code currently makes an error while conducting polynomial interpolation in QAP part.
+% The problem is in inaccure numerical computations due to a high order interpolation.
+% A solution will be to make use of FFT roots of target polynomial. 
+
+c_input=[2 4];
+
+%% From program to R1CS
+Ind_pub=[];
+res=sym('res',[0 0]);
+res(1)='1';
+Ind_pub(end+1)=1;
+A=[];
+B=[];
+C=[];
+
+column_ind=0;
+
+subterms=children(expand(y(1)));
+
+for i=1:length(subterms)
+    factors=factor(subterms(i));
+    lastcum=factors(1);
+    for j=2:length(factors)
+        column_ind=column_ind+1;
+        
+        if ~findfactor(res,lastcum) && has(lastcum,c)
+            res(end+1)=lastcum;
+        end
+        if has(lastcum,c)
+            A(findfactor(res,lastcum),column_ind)=lastcum/res(findfactor(res,lastcum));
+        else
+            A(1,column_ind)=lastcum;
+        end
+        
+        if ~findfactor(res,factors(j)) && has(factors(j),c)
+            res(end+1)=factors(j);
+        end
+        if has(factors(j),c)
+            B(findfactor(res,factors(j)),column_ind)=factors(j)/res(findfactor(res,factors(j)));
+        else
+            B(1,column_ind)=factors(j);
+        end
+        
+        lastcum=lastcum*factors(j);
+        
+        if ~findfactor(res,lastcum) && has(lastcum,c)
+            res(end+1)=lastcum;
+        end
+        if has(lastcum,c)
+            C(findfactor(res,lastcum),column_ind)=lastcum/res(findfactor(res,lastcum));
+        else
+            C(1,column_ind)=lastcum;
+        end
+    end
+end
+
+column_ind=column_ind+1;
+row_inds=zeros(1,length(subterms));
+for i=1:length(subterms)
+    row_inds(i)=findfactor(res,subterms(i));
+end
+A(row_inds,column_ind)=subterms./res(row_inds);
+B(1,column_ind)=1;
+res(end+1)=expand(y(1));
+Ind_pub(end+1)=length(res);
+C(length(res),column_ind)=1;
+
+Ind_pri=setdiff(1:length(res),Ind_pub);
+
+A=[A;zeros(length(res)-size(A,1),size(A,2))];
+B=[B;zeros(length(res)-size(B,1),size(B,2))];
+C=[C;zeros(length(res)-size(C,1),size(C,2))];
+R=subs(res,c,c_input);
+
+% # of wires= size of circuit (m)
+NumWires=size(A,1);
+% # of gates= degree of circuit (d)
+NumGates=size(A,2);
+
+%% Debuging code: Check the matices A,B,C well represent the program.
+deb_inputs=res*A.*(res*B);
+deb_outputs=res*C;
+if ~prod(double(deb_inputs==deb_outputs))==1 || ~double(deb_outputs(end)==expand(y(1)))
+    disp('R1CS has an error.')
+end
+
+
+%% From program to R1CS
+
+% A=[[0, 1, 0, 0, 0, 0];
+% [0, 0, 0, 1, 0, 0];
+% [0, 1, 0, 0, 1, 0];
+% [5, 0, 0, 0, 0, 1]].';
+% B=[[0, 1, 0, 0, 0, 0];
+% [0, 1, 0, 0, 0, 0];
+% [1, 0, 0, 0, 0, 0];
+% [1, 0, 0, 0, 0, 0]].';
+% C=[[0, 0, 0, 1, 0, 0];
+% [0, 0, 0, 0, 1, 0];
+% [0, 0, 0, 0, 0, 1];
+% [0, 0, 1, 0, 0, 0]].';
+% 
+% % # of wires= size of circuit (m)
+% NumWires=size(A,1);
+% % # of gates= degree of circuit (d)
+% NumGates=size(A,2);
+% 
+% Roots_of_Z=1:NumGates;
+% Z=[24, -50, 35, -10, 1];
+% R=[1, 3, 35, 9, 27, 30];
+% % 1 and 3 are inputs; 3 is the output; the rests are intermediate values
+% Ind_mid=[4 5 6];
+% Ind_IO=[1 2 3];
+
+
+%% From R1CS to QAP
+syms x
+
+Roots_of_Z=1:NumGates;
+Zx=1;
+for i=1:NumGates
+    Zx=Zx*(x-Roots_of_Z(i));
+end
+Zp=double(coeffs(Zx));
+
+Ap=zeros(NumWires,NumGates);
+Bp=Ap;
+Cp=Bp;
+
+VANDER=fliplr(vander(Roots_of_Z));
+for i=1:NumWires
+    y_vec=A(i,:).';
+    polycoeffs=(VANDER\y_vec).';
+    Ap(i,:)=polycoeffs;
+end
+
+for i=1:NumWires
+    y_vec=B(i,:).';
+    polycoeffs=(VANDER\y_vec).';
+    Bp(i,:)=polycoeffs;
+end
+
+for i=1:NumWires
+    y_vec=C(i,:).';
+    polycoeffs=(VANDER\y_vec).';
+    Cp(i,:)=polycoeffs;
+end
+
+
+
+% Ap=[[-5.0, 9.166666666666666, -5.0, 0.8333333333333334];
+% [8.0, -11.333333333333332, 5.0, -0.6666666666666666];
+% [0.0, 0.0, 0.0, 0.0];
+% [-6.0, 9.5, -4.0, 0.5];
+% [4.0, -7.0, 3.5, -0.5];
+% [-1.0, 1.8333333333333333, -1.0, 0.16666666666666666]];
+% 
+% Bp=[[3.0, -5.166666666666667, 2.5, -0.33333333333333337];
+% [-2.0, 5.166666666666667, -2.5, 0.33333333333333337];
+% [0.0, 0.0, 0.0, 0.0];
+% [0.0, 0.0, 0.0, 0.0];
+% [0.0, 0.0, 0.0, 0.0];
+% [0.0, 0.0, 0.0, 0.0]];
+% 
+% Cp=[[0.0, 0.0, 0.0, 0.0];
+% [0.0, 0.0, 0.0, 0.0];
+% [-1.0, 1.8333333333333333, -1.0, 0.16666666666666666];
+% [4.0, -4.333333333333333, 1.5, -0.16666666666666666];
+% [-6.0, 9.5, -4.0, 0.5];
+% [4.0, -7.0, 3.5, -0.5]];
+
+
+
+
+[NUM,DEN]=rat([Ap;Bp;Cp]);
+denoms=unique(DEN);
+mul=1;
+for i=1:length(denoms)
+    mul=lcm(mul,denoms(i));
+end
+Ap=round(Ap*mul);
+Bp=round(Bp*mul);
+Cp=round(Cp*mul^2);
+Ap=rmod(Ap,q);
+Bp=rmod(Bp,q);
+Cp=rmod(Cp,q);
+% Zorig=Zp;
+Zp=rmod(Zp,q);
+R=rmod(R,q);
+
+% generate polynomials in variable x from their coefficient vectors
+xv=[];
+for k=0:NumGates-1
+    xv=[xv; x^k];
+end
+Ax=Ap*xv;
+Bx=Bp*xv;
+Cx=Cp*xv;
+
+
+
+% xv=[xv;x^NumGates];
+% Zx=Zp*xv;
+% Zx_orig=Zorig*xv;
+% Zx_roots=double(root(Zx_orig));
+
+% P(x)=linear_combination(r,Ax)*linear_combination(r,Bx)-linear_combination(r,Cx)
+Px=(R*Ax)*(R*Bx)-(R*Cx);
+Px=simplify(Px);
+Px=pmod(Px,q);
+
+%% Debuging code: Check the validity of P(x)
+checksum=0;
+for i=Roots_of_Z
+    checksum=checksum+mod(double(subs(Px,x,i)),q);
+end
+display('QAP result:')
+if checksum==0
+    display('Px is valid')
+else
+    display('Invalid Px, check the Lagrange interpolation')
+end
+
+% find H(x) such that P(x)=H(x)*Z(x)
+[Hx, rem]=polydiv(Px,Zx,q);
+Hx=simplify(Hx);
+Hx=pmod(Hx,q);
+Hp=coeffs(Hx);
+
+if rem==sym(0)
+    display('Hx is valid')
+else
+    display('Invalid Hx')
+end
+
+%% Elliptic curve generation
 Curve=struct();
 Curve.q=p;
 Curve.k=1;
@@ -53,120 +298,7 @@ PointInf2.y=inf;
 %     En(i,:)=EC_add(En(i-1,:),g,a,q);
 % end
 
-%% From function to R1CS
-
-A=[[0, 1, 0, 0, 0, 0];
-[0, 0, 0, 1, 0, 0];
-[0, 1, 0, 0, 1, 0];
-[5, 0, 0, 0, 0, 1]].';
-B=[[0, 1, 0, 0, 0, 0];
-[0, 1, 0, 0, 0, 0];
-[1, 0, 0, 0, 0, 0];
-[1, 0, 0, 0, 0, 0]].';
-C=[[0, 0, 0, 1, 0, 0];
-[0, 0, 0, 0, 1, 0];
-[0, 0, 0, 0, 0, 1];
-[0, 0, 1, 0, 0, 0]].';
-
-% # of wires= size of circuit (m)
-NumWires=size(A,1);
-% # of gates= degree of circuit (d)
-NumGates=size(A,2);
-
-Roots_of_Z=1:NumGates;
-Z=[24, -50, 35, -10, 1];
-R=[1, 3, 35, 9, 27, 30];
-% 1 and 3 are inputs; 3 is the output; the rests are intermediate values
-Ind_mid=[4 5 6];
-Ind_IO=[1 2 3];
-
-
-%% From R1CS to QAP
-Ap=[[-5.0, 9.166666666666666, -5.0, 0.8333333333333334];
-[8.0, -11.333333333333332, 5.0, -0.6666666666666666];
-[0.0, 0.0, 0.0, 0.0];
-[-6.0, 9.5, -4.0, 0.5];
-[4.0, -7.0, 3.5, -0.5];
-[-1.0, 1.8333333333333333, -1.0, 0.16666666666666666]];
-
-Bp=[[3.0, -5.166666666666667, 2.5, -0.33333333333333337];
-[-2.0, 5.166666666666667, -2.5, 0.33333333333333337];
-[0.0, 0.0, 0.0, 0.0];
-[0.0, 0.0, 0.0, 0.0];
-[0.0, 0.0, 0.0, 0.0];
-[0.0, 0.0, 0.0, 0.0]];
-
-Cp=[[0.0, 0.0, 0.0, 0.0];
-[0.0, 0.0, 0.0, 0.0];
-[-1.0, 1.8333333333333333, -1.0, 0.16666666666666666];
-[4.0, -4.333333333333333, 1.5, -0.16666666666666666];
-[-6.0, 9.5, -4.0, 0.5];
-[4.0, -7.0, 3.5, -0.5]];
-
-[NUM,DEN]=rat([Ap;Bp;Cp]);
-denoms=unique(DEN);
-mul=1;
-for i=1:length(denoms)
-    mul=lcm(mul,denoms(i));
-end
-Ap=Ap*mul;
-Bp=Bp*mul;
-Cp=Cp*mul^2;
-Ap=rmod(Ap,q);
-Bp=rmod(Bp,q);
-Cp=rmod(Cp,q);
-Zorig=Z;
-Z=rmod(Z,q);
-R=rmod(R,q);
-
-m=size(Ap,1);
-d=size(Ap,2);
-
-% generate polynomials in variable x from their coefficient vectors
-syms x
-xv=[];
-for k=0:d-1
-    xv=[xv; x^k];
-end
-Ax=Ap*xv;
-Bx=Bp*xv;
-Cx=Cp*xv;
-
-xv=[xv;x^d];
-Zx=Z*xv;
-Zx_orig=Zorig*xv;
-Zx_roots=double(root(Zx_orig));
-
-% P(x)=linear_combination(r,Ax)*linear_combination(r,Bx)-linear_combination(r,Cx)
-Px=(R*Ax)*(R*Bx)-(R*Cx);
-Px=simplify(Px);
-Px=pmod(Px,q);
-
-% Debuging code: Check the validity of P(x)
-checksum=0;
-for i=Zx_roots
-    checksum=checksum+mod(double(subs(Px,x,i)),q);
-end
-display('QAP result:')
-if checksum==0
-    display('Px is valid')
-else
-    display('Invalid Px, check the Lagrange interpolation')
-end
-
-% find H(x) such that P(x)=H(x)*Z(x)
-[Hx, rem]=polydiv(Px,Zx,q);
-Hx=simplify(Hx);
-Hx=pmod(Hx,q);
-Hp=coeffs(Hx);
-
-if rem==sym(0)
-    display('Hx is valid')
-else
-    display('Invalid Hx')
-end
-
-%% Setup
+%% Groth16 Setup
 alpha=1+randi(q-2);
 beta=1+randi(q-2);
 gamma=1+randi(q-2);
@@ -184,11 +316,11 @@ end
 % tau is secret
 tau=[alpha beta gamma delta x_val];
 
-Ax_val=rmod(double(subs(Ax,x,x_val)),q);
-Bx_val=rmod(double(subs(Bx,x,x_val)),q);
-Cx_val=rmod(double(subs(Cx,x,x_val)),q);
-Zx_val=rmod(double(subs(Zx,x,x_val)),q);
-Hx_val=rmod(double(subs(Hx,x,x_val)),q);
+Ax_val=mod(subs(Ax,x,x_val),q);
+Bx_val=mod(subs(Bx,x,x_val),q);
+Cx_val=mod(subs(Cx,x,x_val),q);
+Zx_val=mod(subs(Zx,x,x_val),q);
+Hx_val=mod(subs(Hx,x,x_val),q);
 
 sigma1_1=[EC_pmult(alpha,g);
     EC_pmult(beta,g);
@@ -200,12 +332,12 @@ for i=0:NumGates-1
 end
 sigma1_3(NumWires)=g;
 VAL=zeros(1,NumWires);
-for i=Ind_IO
+for i=Ind_pub
     VAL(i)=mod((beta*Ax_val(i)+alpha*Bx_val(i)+Cx_val(i))*MODinv(gamma,q),q);
     sigma1_3(i)=EC_pmult(VAL(i),g);
 end
 sigma1_4(NumWires)=g;
-for i=Ind_mid
+for i=Ind_pri
     val=mod((beta*Ax_val(i)+alpha*Bx_val(i)+Cx_val(i))*MODinv(delta,q),q);
     sigma1_4(i)=EC_pmult(val,g);
 end
@@ -232,7 +364,7 @@ else
     disp('Polys are not good')
 end
 
-%% Prove
+%% Groth16 Prove
 % Generate r and s, which are secret
 r=randi(q-1);
 s=randi(q-1);
@@ -272,7 +404,7 @@ temp_Proof_B=EC_add(temp_Proof_B,EC_pmult(s,sigma1_1(3)));
 
 Proof_C=EC_add(EC_pmult(s,Proof_A),EC_pmult(r,temp_Proof_B));
 Proof_C=EC_add(Proof_C,EC_inv(EC_pmult(r,EC_pmult(s,sigma1_1(3)))));
-for i=Ind_mid
+for i=Ind_pri
     Proof_C=EC_add(Proof_C,EC_pmult(R(i),sigma1_4(i)));
 end
 for i=0:NumGates-2
@@ -284,30 +416,27 @@ disp('Proof has made')
 %% Debuging code: Check the completeness of proof
 A=mod(alpha+R*Ax_val+r*delta,q);
 B=mod(beta+R*Bx_val+s*delta,q);
-C=mod(MODinv(delta,q)*(R(Ind_mid)*(beta*Ax_val(Ind_mid)+alpha*Bx_val(Ind_mid)+Cx_val(Ind_mid))+Hx_val*Zx_val)...
+C=mod(MODinv(delta,q)*(R(Ind_pri)*(beta*Ax_val(Ind_pri)+alpha*Bx_val(Ind_pri)+Cx_val(Ind_pri))+Hx_val*Zx_val)...
     +A*s+B*r+mod(-r*s*delta,q),q);
 
 lhs=mod(A*B,q);
 rhs=0;
 rhs=mod(rhs+alpha*beta,q);
-rhs=mod(rhs+gamma*R(Ind_IO)*VAL(Ind_IO).',q);
+rhs=mod(rhs+gamma*R(Ind_pub)*VAL(Ind_pub).',q);
 rhs=mod(rhs+C*delta,q);
 
 proofcheckflag=1;
-tempA=EC_pmult(A,g);
-tempB=EC_pmult(B,h);
-tempC=EC_pmult(C,g);
-proofcheckflag=proofcheckflag*(tempA.x==Proof_A.x)*(tempA.y==Proof_A.y)...
-    *(tempB.x==Proof_B.x)*(tempB.y==Proof_B.y)...
-    *(tempC.x==Proof_C.x)*(tempC.y==Proof_C.y);
-if lhs==rhs && proofcheckflag
+proofcheckflag=proofcheckflag*Peq(Proof_A,EC_pmult(A,g))...
+    *Peq(Proof_B,EC_pmult(B,h))...
+    *Peq(Proof_C,EC_pmult(C,g));
+if lhs==rhs && proofcheckflag==1
     disp('Proof is complete')
 else
     disp('Proof is incomplete')
 end
 
 
-%% Verify
+%% Groth16 Verify
 % modular by p
 % e=@(u,v) cmod(weil(EC_pmult(u,g),EC_pmult(v,h),Group1,Group2),p);
 % e11=e(1,1);
@@ -321,7 +450,7 @@ RHS=1;
 % e(alpha*g, beta*h)
 RHS=cmod(RHS*weil(sigma1_1(1),sigma2_1(1),Group1,Group2),p);
 temp=PointInf1;
-for i=Ind_IO
+for i=Ind_pub
     temp=EC_add(temp,EC_pmult(R(i),sigma1_3(i)));
 end
 RHS=cmod(RHS*weil(temp,sigma2_1(2),Group1,Group2),p);

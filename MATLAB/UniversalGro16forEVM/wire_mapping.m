d5938c716e9ed8294784b0c8979bf51fdfc6c45f
JehyukJang <109242826+JehyukJang@users.noreply.github.com>
2022-09-01 14:11:58 +0900

    Demo version

--- MATLAB/UniversalGro16forEVM/wire_mapping.m
+++ MATLAB/UniversalGro16forEVM/wire_mapping.m
@@ -1,9 +1,34 @@
 function oplist_out=wire_mapping(op, stack_pt, d, a)
 global oplist op_pointer
 oplist(1).opcode='fff';
+% Dividing SHR into SHR-H(high) and SHR-L(low)
+if strcmp(op, '1c')
+    % assumption that inputs from other opcodes' outputs always result in SHR-L
+    digitsOld = digits(77);
+    target_val = eval_EVM(stack_pt(2,:));
+    threshold = vpa(2^248); % 31 bytes precision
+    flag=double(target_val < threshold);
+    digits(digitsOld);
+    shiftamount = eval_EVM(stack_pt(1,:));
+    if flag
+        op='1c1';
+    elseif ~flag && shiftamount>=8
+        op='1c2';
+    else
+        error('SHR: high target value but small shift amount');
+    end
+end
 for i=1:d
     if stack_pt(i,1)==0
         data=stack_pt(i,:);
+        if i==2 && (strcmp(op, '1c1') || strcmp(op, '1c2'))
+            % if SHR-L, read lowest 31 bytes, and if SHR-R, read highest 31 bytes
+            original_bytelength=data(3);
+            data(3) = min(31, original_bytelength);
+            if strcmp(op, '1c1')
+                data(1) = data(1) + max(original_bytelength-data(3),0); % shift offset
+            end
+        end
         if isempty(oplist(1).pt_outputs)
             checks=0;
         else
@@ -14,6 +39,9 @@ for i=1:d
             stack_pt(i,:)=[1 size(oplist(1).pt_outputs,1) 32];
         else
             stack_pt(i,:)=[1 find(checks==1) 32];
+        end
+        if strcmp(op, '20')
+            stack_pt(i,3) = data(3);
         end
     end
 end
